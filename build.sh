269cb15a1b33976bfe8a3c676d3ee80cb0b0091f
Curtis Rueden <ctrueden@wisc.edu>
2018-11-20 16:21:57 -0600

    Tweak build script
    * If no new CSV download, skip moving it
    * Do not delete the render.sh; might be useful afterward
    * If GDrive folder is missing, skip copying PNGs

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-mv ${HOME}/Downloads/ij\ timeline\ -\ Sheet1.csv ./
+
+newsheet="$HOME/Downloads/ij timeline - Sheet1.csv"
+destination="$HOME/Google Drive/I2K-2018/timeline"
+
+if [ -f "$newsheet" ]; then mv "$newsheet" ./; fi
 lua highlights.lua > render.sh
 . render.sh
-rm render.sh
-cp *.png ${HOME}/Google\ Drive/I2K-2018/timeline/
-
+if [ -d "$destination" ]; then cp *.png "$destination/"; fi
